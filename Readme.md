977146499835e46dec47fd095adf6e018b0b8c71
Gustav Larson <gustav.larson@gmail.com>
2016-05-23 19:01:29 +0200

    Change to YAML configuration file and support for multiple domains

--- Readme.md
+++ Readme.md
@@ -9,22 +9,29 @@ Requirements
 Requires ipgetter and python-nfsn
 * ipgetter: https://github.com/phoemur/ipgetter
 * python-nfsn: https://github.com/ktdreyer/python-nfsn
+* PyYAML: http://pyyaml.org/
 
-Install both using pip:
+Install all using pip:
 
-    pip install ipgetter
-    pip install python-nfsn
+    pip install ipgetter python-nfsn pyyaml
 
 Configuration
 ========
 Place a configuration file at `/etc/nfsn_dyndns.conf` with the content
-```json
-{
-  "login": "LOGIN NAME",
-  "api-key": "API KEY",
-  "domain": "example.com"
-}
+```yaml
+login: LOGIN_NAME
+api-key: API_KEY
+domains:
+- domain: example.com
 ```
+
+There is support for multiple domains. Just list all domains under the domains key:
+```yaml
+domains:
+- domain: example.com
+- domain: example.net
+```
+
 
 Usage
 ========
--- nfsn_dyndns.py
+++ nfsn_dyndns.py
@@ -14,7 +14,7 @@ https://github.com/ktdreyer/python-nfsn
 
 """
 from __future__ import print_function
-import json
+import yaml
 import os
 import tempfile
 
@@ -27,39 +27,40 @@ IP_FILE = '{directory}/nfsn_dyndns.ip'.format(directory=tempfile.gettempdir())
 PUBLIC_IP = ipgetter.myip()
 
 with open(CONFIG_FILE) as config_raw:
-    CONFIG = json.load(config_raw)
+    CONFIG = yaml.safe_load(config_raw)
 
-NFSN = Nfsn(login_file=CONFIG_FILE)
+NFSN = Nfsn(login=CONFIG["login"], api_key=CONFIG["api-key"])
 
 
 def main():
     """Main function. Check if update is required and perform update"""
     if update_needed():
         print('DNS record update is needed')
-        domain = CONFIG['domain']
-        records = NFSN.dns(domain).listRRs()
-        for record in records:
-            if record['type'] == 'A' and record['data'] != PUBLIC_IP:
-                print('Removing {name}.{domain} with data: {data}'.format(
-                    domain=domain,
-                    name=record['name'],
-                    data=record['data']
-                    ))
-                NFSN.dns(domain).removeRR(
-                    name=record['name'],
-                    type=record['type'],
-                    data=record['data']
-                    )
-                print('Adding {name}.{domain} with data: {data}'.format(
-                    domain=domain,
-                    name=record['name'],
-                    data=PUBLIC_IP
-                    ))
-                NFSN.dns(domain).addRR(
-                    name=record['name'],
-                    type=record['type'],
-                    data=PUBLIC_IP
-                    )
+        for entry in CONFIG['domains']:
+            domain = entry['domain']
+            records = NFSN.dns(domain).listRRs()
+            for record in records:
+                if check_update_record(record):
+                    print('Removing {name}.{domain} with data: {data}'.format(
+                        domain=domain,
+                        name=record['name'],
+                        data=record['data']
+                        ))
+                    NFSN.dns(domain).removeRR(
+                        name=record['name'],
+                        type=record['type'],
+                        data=record['data']
+                        )
+                    print('Adding {name}.{domain} with data: {data}'.format(
+                        domain=domain,
+                        name=record['name'],
+                        data=PUBLIC_IP
+                        ))
+                    NFSN.dns(domain).addRR(
+                        name=record['name'],
+                        type=record['type'],
+                        data=PUBLIC_IP
+                        )
 
 
 def update_needed():
@@ -90,15 +91,23 @@ def update_needed():
             print('All records does not match public ip')
             return True
 
+def check_update_record(record):
+    """Check if a record needs to be updated."""
+    if record['type'] != 'A' or record['data'] == PUBLIC_IP:
+        return False
+
+    return True
 
 def all_records_up_to_date():
     """Check if all DNS resource records are matching the public ip"""
-    domain = CONFIG['domain']
-    records = NFSN.dns(domain).listRRs()
-    for record in records:
-        if record['type'] == 'A':
-            if record['data'] != PUBLIC_IP:
-                return False
+    for entry in CONFIG['domains']:
+        domain = entry['domain']
+        records = NFSN.dns(domain).listRRs()
+        for record in records:
+            if record['type'] == 'A':
+                if record['data'] != PUBLIC_IP:
+                    return False
+
     return True
 
 if __name__ == '__main__':
